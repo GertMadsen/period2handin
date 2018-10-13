1d5f41ee2c587d7a64be518453a741489134d52f
Gert Lehmann Madsen <31619672+GertMadsen@users.noreply.github.com>
2018-10-13 20:17:22 +0200

    Delete settings.js

--- settings.js
+++
@@ -1,9 +0,0 @@
-const DEV_DB_URI = "mongodb://XXXXXX:XXXXXX@ds125423.mlab.com:25423/miniproject";
-const TEST_DB_URI = "mongodb://XXXXXX:XXXXXX@ds125263.mlab.com:25263/miniprojecttest";
-const MOCHA_TEST_TIMEOUT = 5000;
-
-module.exports = {
-  DEV_DB_URI,
-  TEST_DB_URI,
-  MOCHA_TEST_TIMEOUT
-}
