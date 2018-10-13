bddc004640c88ff245aace0c427fbd42da309640
GertMadsen <cph-gm50@cphbusiness.dk>
2018-10-13 23:38:04 +0200

    trying to add test for REST api

--- test/testRestApi.js
+++ test/testRestApi.js
@@ -1,13 +1,40 @@
 const expect = require("chai").expect;
+const request = require('request');
+const http = require('http')
+const app = require('../app');
+
+var server = null;
+const TEST_PORT = 3456;
 
 
-describe('app', function(){
-    var app = require('../app');
-    beforeEach(function(){
-      app.listen(3000);
+describe('Testing REST API', function () {
+  before(function (done) {
+    server = http.createServer(app);
+    server.listen(TEST_PORT, function () {
+      done();
     });
-
-    afterEach(function(){
-      app.close();
-    })
   });
+
+  it('Testing server connection', function () {
+    expect(server.listening).to.be.true;
+  });
+
+  it('Testing /users REST endpoint', function (done) {
+    const options = {
+      url: 'http://localhost:' + TEST_PORT + '/users',
+      method: 'GET',
+      json: true
+    };
+
+    request(options, function (error, res, body) {
+      console.log(body)
+    })
+    done()
+  });
+
+
+  after(function (done) {
+    server.close();
+    done();
+  });
+});
